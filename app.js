91c5998b291177616bceb25b244a3eaa9deac0c4
adriansito <adriancrosby02@gmail.com>
2026-01-19 19:14:04 -0800

    configured tailwindcss for signup form

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ require('dotenv').config();
 const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
